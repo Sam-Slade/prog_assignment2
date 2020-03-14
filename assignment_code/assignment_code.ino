d10ba4313221af6c2f6071ec33dccc03906d8e22
MrSpectra <sam@samslade.co.uk>
2020-03-14 12:36:06 +0000

    Added spi lcd and got the keypad working

--- assignment_code/assignment_code.ino
+++ assignment_code/assignment_code.ino
@@ -1,22 +1,25 @@
 // Include header files
 #include <Keypad.h> //4x4 keypad header file
 #include <Servo.h> //Servo motor header file
+#include <SPI.h>
+#include <LiquidCrystal.h>
 
 //Servo motor setup
-const int SERVO_PIN = 9;
+const int SERVO_PIN = 2;
 Servo Servo1;
 
 //Keypad setup code
-const int ROWS = 4;
-const int COLS = 4;
+const byte ROWS = 4;
+const byte COLS = 4;
 
-char keymap[ROWS][COLS] = {{'1','2','3','A'},
-{'4','5','6','B'},
-{'7','8','9','C'},
-{'*','0','#','D'}};
+char keymap[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}};
 
-byte rowPins[ROWS] = {7,6,5,4};
-byte colPins[COLS] = {3,2,1,0};
+byte rowPins[ROWS] = {12,8,7,6};
+byte colPins[COLS] = {5,4,3,2};
 
 Keypad keypad = Keypad(makeKeymap(keymap), rowPins, colPins, ROWS, COLS);
 
@@ -25,6 +28,8 @@ void buzzer_success(); // Login Success buzzer tone
 void buzzer_fail(); // Login Fail buzzer tone
 const int BUZZER = 10; // Set buzzer pin
 
+// lcd screen
+LiquidCrystal lcd(9);
 
 //User input class
 class userCode {
@@ -56,11 +61,13 @@ class userCode {
 };
 
 void setup() {
+  Serial.begin(9600);
   //Set buzzer pin to output
   pinMode(BUZZER, OUTPUT);
 
   //Set servo pin to output
   Servo1.attach(SERVO_PIN);
+  Servo1.writeMicroseconds(180);
 
   //Declare struct for user code
   ::userCode code;
@@ -68,35 +75,34 @@ void setup() {
     
   //Add event listener to the keypad
   //to catch user input
-  keypad.addEventListener(userInput);
+  //keypad.addEventListener(keypadEvent);
 }
 
 
 void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    lcd.setCursor(0,0);
+    lcd.print(key);
+    buzzer_success();
+  }
 }
 
 void buzzer_success() {
   tone(BUZZER, 2500);
-  delay(500);
+  delay(200);
   noTone(BUZZER);
 }
 
 void buzzer_fail() {
   tone(BUZZER, 450);
-  delay(500);
+  delay(200);
   noTone(BUZZER);
 }
 
-
-
-//Listen for user input and store it
-void userInput(KeypadEvent key, userCode code){
-  switch (keypad.getState()) {
-  case PRESSED:
-    if (key == 'c') {
-      code.set();
-    } else {
-      code.enterDigit(key);
-    }
-  }
+void buzzer_tap() {
+  tone(BUZZER, 2500);
+  delay(10);
+  noTone(BUZZER);
 }
